c69c4a294c1e8980693a0017bc3b40ee062e1fca
rtr1792 <rtr1792@gmail.com>
2018-01-27 09:34:51 -0600

    Add files via upload

---
+++ subclassDriveCode/src/Robot.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include "WPILib.h"
+#include "ctre/Phoenix.h"
+#include <Joystick.h>
+#include <IterativeRobot.h>
+#include <LiveWindow/LiveWindow.h>
+#include <SmartDashboard/SendableChooser.h>
+#include <SmartDashboard/SmartDashboard.h>
+#include "Drive/DifferentialDrive.h"
+#include "DriverStation.h"
+#include <Segments/drive.h>
+
+
+class Robot : public frc::IterativeRobot {
+
+public:
+	Robot() {
+	this->driveManager = new DriveManager();
+	}
+private:
+	DriveManager *driveManager;
+
+
+	void RobotInit() {
+		m_chooser.AddDefault(kAutoNameDefault, kAutoNameDefault);
+		m_chooser.AddObject(kAutoNameCustom, kAutoNameCustom);
+		frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+	}
+
+
+	void AutonomousInit() override {
+		m_autoSelected = m_chooser.GetSelected();
+		// m_autoSelected = SmartDashboard::GetString(
+		// 		"Auto Selector", kAutoNameDefault);
+		std::cout << "Auto selected: " << m_autoSelected << std::endl;
+
+		if (m_autoSelected == kAutoNameCustom) {
+			// Custom Auto goes here
+		} else {
+			// Default Auto goes here
+		}
+	}
+
+	void AutonomousPeriodic() {
+		if (m_autoSelected == kAutoNameCustom) {
+			// Custom Auto goes here
+		} else {
+			// Default Auto goes here
+		}
+	}
+
+	void TeleopInit() {}
+
+	void TeleopPeriodic() {
+		this->driveManager->driveTrain();
+	}
+
+	void TestPeriodic() {}
+
+private:
+	frc::LiveWindow& m_lw = *LiveWindow::GetInstance();
+	frc::SendableChooser<std::string> m_chooser;
+	const std::string kAutoNameDefault = "Default";
+	const std::string kAutoNameCustom = "My Auto";
+	std::string m_autoSelected;
+};
+
+START_ROBOT_CLASS(Robot)
